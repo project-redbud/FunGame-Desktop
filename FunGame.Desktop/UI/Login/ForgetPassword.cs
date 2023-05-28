113986bfdf0abbfaf2a7d1c8ae4230cd9f972a5c
Mili <mili@mili.cyou>
2023-05-28 22:59:58 +0800

    完成ForgetPassword 第一部分

--- FunGame.Desktop/UI/Login/ForgetPassword.cs
+++ FunGame.Desktop/UI/Login/ForgetPassword.cs
@@ -1,5 +1,7 @@
 ﻿using Milimoe.FunGame.Core.Api.Transmittal;
 using Milimoe.FunGame.Core.Library.Constant;
+using Milimoe.FunGame.Core.Library.Exception;
+using Milimoe.FunGame.Core.Library.SQLScript.Common;
 using Milimoe.FunGame.Core.Library.SQLScript.Entity;
 using Milimoe.FunGame.Desktop.Library;
 using Milimoe.FunGame.Desktop.Library.Component;
@@ -31,20 +33,88 @@ namespace Milimoe.FunGame.Desktop.UI
                     return;
                 }
 
-                bool finding = false;
+                string msg = "";
+                bool success = false;
 
-                DataRequest request = RunTime.NewDataRequest(DataRequestType.GetFindPasswordVerifyCode);
-                request.AddRequestData(UserQuery.Column_Username, username);
-                request.AddRequestData(UserQuery.Column_Email, email);
-                request.SendRequest();
-                if (request.Result == RequestResult.Success)
+                try
                 {
-                    finding = request.GetResult<bool>("finding");
-                    ShowMessage.TipMessage(finding.ToString());
+                    DataRequest request = RunTime.NewDataRequest(DataRequestType.GetFindPasswordVerifyCode);
+                    request.AddRequestData(UserQuery.Column_Username, username);
+                    request.AddRequestData(UserQuery.Column_Email, email);
+                    request.AddRequestData(ForgetVerifyCodes.Column_ForgetVerifyCode, "");
+                    request.SendRequest();
+                    if (request.Result == RequestResult.Success)
+                    {
+                        msg = request.GetResult<string>("msg") ?? "";
+                        if (msg.Trim() != "")
+                        {
+                            // 如果返回一个信息，则停止找回密码
+                            ShowMessage.ErrorMessage(msg);
+                        }
+                        else
+                        {
+                            while (!success)
+                            {
+                                request[ForgetVerifyCodes.Column_ForgetVerifyCode] = "";
+                                string verifycode = ShowMessage.InputMessageCancel("请输入找回密码邮件中的6位数字验证码", "注册验证码", out MessageResult result);
+                                if (result != MessageResult.Cancel)
+                                {
+                                    if (verifycode.Trim() != "")
+                                    {
+                                        request[ForgetVerifyCodes.Column_ForgetVerifyCode] = verifycode;
+                                        request.SendRequest();
+                                        if (request.Result == RequestResult.Success)
+                                        {
+                                            msg = request.GetResult<string>("msg") ?? "";
+                                            if (msg.Trim() != "")
+                                            {
+                                                ShowMessage.ErrorMessage(msg);
+                                            }
+                                            else
+                                            {
+                                                success = true;
+                                                break;
+                                            }
+                                        }
+                                        else
+                                        {
+                                            RunTime.WritelnSystemInfo(request.Error);
+                                        }
+                                    }
+                                    else
+                                    {
+                                        ShowMessage.WarningMessage("不能输入空值！");
+                                    }
+                                }
+                                else break;
+                            }
+                            if (success)
+                            {
+                                bool checkpass = true;
+                                while (checkpass)
+                                {
+                                    string newpass = ShowMessage.InputMessageCancel("请输入新密码", "新密码", out MessageResult result);
+                                    if (newpass.Trim() != "")
+                                    {
+                                        if (newpass.Length < 6 || newpass.Length > 15) // 字节范围 3~12
+                                        {
+                                            ShowMessage.ErrorMessage("密码长度不符合要求：6~15个字符数");
+                                        }
+                                        else checkpass = false;
+                                    }
+                                }
+                                // TODO. 等更新UpdatePassword
+                            }
+                        }
+                    }
+                    else
+                    {
+                        RunTime.WritelnSystemInfo(request.Error);
+                    }
                 }
-                else
+                catch (Exception ex)
                 {
-                    RunTime.WritelnSystemInfo(request.Error);
+                    RunTime.WritelnSystemInfo(ex.GetErrorInfo());
                 }
             }
         }
