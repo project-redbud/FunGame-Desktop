2fbb511ea779eb659e24b02de7cfabe6ad6c38aa
milimoe <110188673+milimoe@users.noreply.github.com>
2023-11-19 14:54:18 +0800

    add -debug launcher option

--- FunGame.Desktop/UI/Start.cs
+++ FunGame.Desktop/UI/Start.cs
@@ -6,8 +6,17 @@ namespace Milimoe.FunGame.Desktop.UI
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main(params string[] args)
         {
+            foreach (string option in args)
+            {
+                switch (option)
+                {
+                    case "-debug":
+                        Core.Library.Constant.FunGameInfo.FunGame_DebugMode = true;
+                        break;
+                }
+            }
             ApplicationConfiguration.Initialize();
             Application.Run(new Main());
         }
